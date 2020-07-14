3990f19f1415e9e583bf9522e70936598cec3a6b
Itamar Pereira <itamartpereira@hotmail.com>
2020-07-13 23:08:47 -0300

    Update customer spec with capitalize test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  let(:valid_attributes) { { name: 'alan peter' } }
+  subject { Customer.new(valid_attributes) }
+
   context 'relationships' do
     it { is_expected.to have_many :carts }
   end
+
+  context 'name' do
+    it 'capitalize' do
+      expect(subject.titleize_name).to eql 'Alan Peter'
+    end
+  end
+
 end
